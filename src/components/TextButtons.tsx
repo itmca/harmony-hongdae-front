bee006b3c9435c455e010109a362a1ab41609226
solmioh <solshine_@hanmail.net>
2021-09-12 20:29:02 +0900

    props

--- src/components/TextButtons.tsx
+++ src/components/TextButtons.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import MTable from './MTable';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStylesButton = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       '& > *': {
@@ -12,18 +13,28 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+function showValue(e:any) {
+  const value = e.currentTarget.value;
+  console.log(value);
+  return (
+    <div>
+      <MTable menuUrl={value} />
+    </div>
+  );
+}
+
 export default function TextButtons() {
-  const classes = useStyles();
+  const classes = useStylesButton();
 
   return (
     <div className={classes.root}>
-      <Button color="secondary">ALL</Button>
-      <Button>Korean</Button>
-      <Button>West</Button>
-      <Button>Chinese</Button>
-      <Button>Japanese</Button>
-      <Button>Mexican</Button>
-      <Button>Dessert</Button>
+      <Button color="secondary" value="http://localhost:5000/basic" onClick={e => showValue(e)}>ALL</Button>
+      <Button value="http://localhost:5000/basic/korean" onClick={e => showValue(e)}>Korean</Button>
+      <Button value="http://localhost:5000/basic/west" onClick={e => showValue(e)}>West</Button>
+      <Button value="http://localhost:5000/basic/chinese" onClick={e => showValue(e)}>Chinese</Button>
+      <Button value="http://localhost:5000/basic/japanese" onClick={e => showValue(e)}>Japanese</Button>
+      <Button value="http://localhost:5000/basic/mexican" onClick={e => showValue(e)}>Mexican</Button>
+      <Button value="http://localhost:5000/basic/dessert" onClick={e => showValue(e)}>Dessert</Button>
     </div>
   );
 }
